560b940a9a68f285aa3a594c4803f60d53bfe655
Aaron Housh <dispersias@gmail.com>
2020-03-28 12:52:28 -0700

    Update liveness url

--- api/src/routes/v1/status.rs
+++ api/src/routes/v1/status.rs
@@ -3,6 +3,5 @@ use actix_web::{get, HttpResponse};
 #[get("api/v1/status")]
 pub fn get_status() -> HttpResponse {
     HttpResponse::Ok()
-        .content_type("application/json")
-        .body(r#"{"status": true}"#)
+        .finish()
 }
